ec555f5a390932d91ede58f8b315681e71583b4b
Sawan Gupta <sawan.gupta92@gmail.com>
2016-10-31 02:04:20 +0530

    Add details to wine page

--- src/Wine.js
+++ src/Wine.js
@@ -6,10 +6,29 @@ class Wine extends Component {
   render() {
     return (
       <div className="Wine">
-      fdsmklfmdsklfm
         { this.props.wines && this.props.wines.map((row) => {
-          return <div key={row.id}>
-            { row.allocation_type }
+          return <div key={row.id} className='col-lg-12'>
+            <div className='col-lg-4 row'>
+              <img src={row.image.large} className="wine-image" alt='wine' />
+            </div>
+            <div className='col-lg-4 row'>
+              Allocation Type: { row.allocation_type }
+            </div>
+            <div className='col-lg-4 row'>
+              Alcohol Percentage: { row.alcohol_percentage }
+            </div>
+            <div className='col-lg-4 row'>
+              Bottle Size: { row.bottle_size }
+            </div>
+            <div className='col-lg-4 row'>
+              Vintage: { row.vintage }
+            </div>
+            <div className='col-lg-4 row'>
+              Wine Name: { row.wine_name }
+            </div>
+            <div className='col-lg-4 row'>
+              Wine Description: { row.wine_description }
+            </div>
           </div>
         }) }
       </div>
